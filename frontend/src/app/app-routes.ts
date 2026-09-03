6263dda94962168e3b5bb3ecf5272b97ab18a8d5
Mathieu <126501525+Marchat9@users.noreply.github.com>
2026-09-03 11:05:34 +0200

    fix bundle size

--- frontend/src/app/app-routes.ts
+++ frontend/src/app/app-routes.ts
@@ -1,26 +1,69 @@
 import { Routes } from '@angular/router';
-import { AdminTournamentPageComponent } from './pages/admin-tournament-page/admin-tournament-page.component';
-import { HomePageComponent } from './pages/home-page/home-page';
-import { PlayerTeamMatchPageComponent } from './pages/player-team-match-page/player-team-match-page';
-import { TournamentCreatePageComponent } from './pages/tournament-create-page/tournament-create-page';
-import { AdminConnectionPageComponent } from './pages/admin-connection-page/admin-connection-page';
-import { PlayerConnectionPage } from './pages/player-connection-page/player-connection-page';
-import { SpectatorConnectionPageComponent } from 'src/app/pages/spectator-connection-page/spectator-connection-page';
-import { SpectatorPageComponent } from 'src/app/pages/spectator-page/spectator-page';
-import { FriendlyMatchPageComponent } from './pages/friendly-match-page/friendly-match-page';
-import { SuperAdminPageComponent } from './pages/super-admin-page/super-admin-page';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'accueil' },
-  { path: 'accueil', component: HomePageComponent },
-  { path: 'admin/tournament-creation', component: TournamentCreatePageComponent },
-  { path: 'admin', component: AdminConnectionPageComponent },
-  { path: 'admin/:tournamentCode', component: AdminTournamentPageComponent },
-  { path: 'player', component: PlayerConnectionPage },
-  { path: 'player/:tournamentCode/:teamCode', component: PlayerTeamMatchPageComponent },
-  { path: 'spectateur', component: SpectatorConnectionPageComponent },
-  { path: 'spectateur/:tournamentCode', component: SpectatorPageComponent },
-  { path: 'friendly-match', component: FriendlyMatchPageComponent },
-  { path: 'super-admin', component: SuperAdminPageComponent },
+  {
+    path: 'accueil',
+    loadComponent: () => import('./pages/home-page/home-page').then((m) => m.HomePageComponent),
+  },
+  {
+    path: 'admin/tournament-creation',
+    loadComponent: () =>
+      import('./pages/tournament-create-page/tournament-create-page').then(
+        (m) => m.TournamentCreatePageComponent,
+      ),
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./pages/admin-connection-page/admin-connection-page').then(
+        (m) => m.AdminConnectionPageComponent,
+      ),
+  },
+  {
+    path: 'admin/:tournamentCode',
+    loadComponent: () =>
+      import('./pages/admin-tournament-page/admin-tournament-page.component').then(
+        (m) => m.AdminTournamentPageComponent,
+      ),
+  },
+  {
+    path: 'player',
+    loadComponent: () =>
+      import('./pages/player-connection-page/player-connection-page').then(
+        (m) => m.PlayerConnectionPage,
+      ),
+  },
+  {
+    path: 'player/:tournamentCode/:teamCode',
+    loadComponent: () =>
+      import('./pages/player-team-match-page/player-team-match-page').then(
+        (m) => m.PlayerTeamMatchPageComponent,
+      ),
+  },
+  {
+    path: 'spectateur',
+    loadComponent: () =>
+      import('./pages/spectator-connection-page/spectator-connection-page').then(
+        (m) => m.SpectatorConnectionPageComponent,
+      ),
+  },
+  {
+    path: 'spectateur/:tournamentCode',
+    loadComponent: () =>
+      import('./pages/spectator-page/spectator-page').then((m) => m.SpectatorPageComponent),
+  },
+  {
+    path: 'friendly-match',
+    loadComponent: () =>
+      import('./pages/friendly-match-page/friendly-match-page').then(
+        (m) => m.FriendlyMatchPageComponent,
+      ),
+  },
+  {
+    path: 'super-admin',
+    loadComponent: () =>
+      import('./pages/super-admin-page/super-admin-page').then((m) => m.SuperAdminPageComponent),
+  },
   { path: '**', redirectTo: 'accueil' },
 ];
